83eddb76c490aa215b5802424410dcbf16604287
azarrias <adolfo.zarrias@gmail.com>
2020-07-22 12:20:42 +0200

    Create ground body towards the bottom of the screen so that the other body bounces off

--- src/main.lua
+++ src/main.lua
@@ -27,13 +27,23 @@ function love.load()
   world = love.physics.newWorld(0, 300)
 
   -- body that stores velocity and position and all fixtures
-  boxBody = love.physics.newBody(world, VIRTUAL_SIZE.x / 2, VIRTUAL_SIZE.y / 2, 'static')
+  boxBody = love.physics.newBody(world, VIRTUAL_SIZE.x / 2, VIRTUAL_SIZE.y / 2, 'dynamic')
 
   -- shape that we will attach using a fixture to our body for collision detection
   boxShape = love.physics.newRectangleShape(10, 10)
 
   -- fixture that attaches a shape to our body
   boxFixture = love.physics.newFixture(boxBody, boxShape)
+  boxFixture:setRestitution(0.5) -- defines "bounciness"
+  
+  -- static ground body
+  groundBody = love.physics.newBody(world, 0, VIRTUAL_SIZE.y - 30, 'static')
+  
+  -- edge shape Box2D provides, perfect for ground
+  edgeShape = love.physics.newEdgeShape(0, 0, VIRTUAL_SIZE.x, 0)
+  
+  -- affix edge shape to our body
+  groundFixture = love.physics.newFixture(groundBody, edgeShape)
   
   love.keyboard.keysPressed = {}
 end
@@ -65,7 +75,13 @@ function love.draw()
   
   -- draw a polygon shape by getting the world points for our body, using the box shape's
   -- definition as a reference
+  love.graphics.setColor(0, 1, 0, 1)
   love.graphics.polygon('fill', boxBody:getWorldPoints(boxShape:getPoints()))
+  
+  -- draw a line that represents our ground, calculated from ground body and edge shape
+  love.graphics.setColor(1, 0, 0, 1)
+  love.graphics.setLineWidth(2)
+  love.graphics.line(groundBody:getWorldPoints(edgeShape:getPoints()))
   
   push:finish()
 end
